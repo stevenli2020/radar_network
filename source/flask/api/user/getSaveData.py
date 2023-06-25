21be2dfc25ecd5043a38517e5c76c4e2681bb802
steven2020 <steven2020@github.com>
2023-06-25 14:45:09 +0000

    auto save

--- source/flask/api/user/getSaveData.py
+++ source/flask/api/user/getSaveData.py
@@ -324,11 +324,14 @@ def getSummaryDataofPosition(data):
         timeRange = "1 WEEK"
     else:
         timeRange = "1 MONTH"
-    sql = "SELECT * FROM Gaitmetrics.PROCESSED_DATA;"
+
+    sql = "SELECT ROOM_X*%d AS X_RANGE,ROOM_Y*%d AS Y_RANGE FROM ROOMS_DETAILS RIGHT JOIN RL_ROOM_MAC ON ROOMS_DETAILS.ROOM_UUID=RL_ROOM_MAC.ROOM_UUID WHERE RL_ROOM_MAC.MAC='%s';" %(N, N, data['DEVICEMAC'])
     cursor.execute(sql)
-    # dbresult = cursor.fetchone()    
-    # X_RANGE = int(dbresult[0])
-    # Y_RANGE = int(dbresult[1])        
+    dbresult = cursor.fetchone()    
+    X_RANGE = int(dbresult[0])
+    Y_RANGE = int(dbresult[1]) 
+
+       
     sql = "SELECT ROUND((MAX(PX)-MIN(PX)),1) AS DELTA_X,ROUND((MAX(PY)-MIN(PY)),1) AS DELTA_Y FROM Gaitmetrics.PROCESSED_DATA WHERE MAC='%s' AND `TIMESTAMP` > DATE_SUB(NOW(), INTERVAL %s) AND `PX` IS NOT NULL AND PY IS NOT NULL;" %(data['DEVICEMAC'], timeRange)
     cursor.execute(sql)
     dbresult = cursor.fetchone() 

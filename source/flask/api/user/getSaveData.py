2c3fbe6ddc9b66540e051013ff3d7e849321a61e
steven2020 <steven2020@github.com>
2023-06-25 14:29:57 +0000

    auto save

--- source/flask/api/user/getSaveData.py
+++ source/flask/api/user/getSaveData.py
@@ -323,12 +323,7 @@ def getSummaryDataofPosition(data):
     elif t == "WEEK":
         timeRange = "1 WEEK"
     else:
-        timeRange = "1 MONTH"
-    sql = "SELECT ROOM_X*%d AS X_RANGE,ROOM_Y*%d AS Y_RANGE FROM `ROOMS_DETAILS` RIGHT JOIN RL_ROOM_MAC ON ROOMS_DETAILS.ROOM_UUID=RL_ROOM_MAC.ROOM_UUID WHERE RL_ROOM_MAC.MAC='%s';" %(N,N,data['DEVICEMAC'])
-    cursor.execute(sql)
-    # dbresult = cursor.fetchone()    
-    # X_RANGE = int(dbresult[0])
-    # Y_RANGE = int(dbresult[1])        
+        timeRange = "1 MONTH"     
     sql = "SELECT ROUND((MAX(PX)-MIN(PX)),1) AS DELTA_X,ROUND((MAX(PY)-MIN(PY)),1) AS DELTA_Y FROM Gaitmetrics.PROCESSED_DATA WHERE MAC='%s' AND `TIMESTAMP` > DATE_SUB(NOW(), INTERVAL %s) AND `PX` IS NOT NULL AND PY IS NOT NULL;" %(data['DEVICEMAC'], timeRange)
     cursor.execute(sql)
     dbresult = cursor.fetchone() 
@@ -346,6 +341,12 @@ def getSummaryDataofPosition(data):
 
     HMAP = np.zeros((X_SIZE, Y_SIZE))
     HMAP2 = np.zeros((X_SHIFT, X_SHIFT))
+
+    sql = "SELECT ROOM_X*10 AS X_RANGE,ROOM_Y*10 AS Y_RANGE FROM ROOMS_DETAILS RIGHT JOIN RL_ROOM_MAC ON ROOMS_DETAILS.ROOM_UUID=RL_ROOM_MAC.ROOM_UUID WHERE RL_ROOM_MAC.MAC='%s';" % (data['DEVICEMAC'])
+    cursor.execute(sql)
+    # dbresult = cursor.fetchone()    
+    # X_RANGE = int(dbresult[0])
+    # Y_RANGE = int(dbresult[1])   
         
 
     sql = "SELECT CONCAT(ROUND(PX*%d),',',ROUND(PY*%d)) AS XY,COUNT(*) AS CNT FROM Gaitmetrics.PROCESSED_DATA WHERE MAC='%s' AND `TIMESTAMP` > DATE_SUB(NOW(), INTERVAL %s) AND `PX` IS NOT NULL AND `PY` IS NOT NULL GROUP BY XY ORDER BY XY ASC;" %(N, N, data['DEVICEMAC'], timeRange)

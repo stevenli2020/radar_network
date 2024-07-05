0a8217944d2ba367593b392eddcedca08f6f85b1
mvplcw <limcheewei4727_@hotmail.com>
2024-07-05 04:33:25 +0000

    auto save

--- source/decode_publish/app/runBack.py
+++ source/decode_publish/app/runBack.py
@@ -172,13 +172,6 @@ def decode_process_publish(mac, data):
 
                 # print(deltaT)
                 if deltaT > 100:
-                    wallStateParam[mac]['x0'] = np.nan
-                    wallStateParam[mac]['y0'] = np.nan
-                    wallStateParam[mac]['z0'] = np.nan
-                    wallStateParam[mac]['timeStamp_stationary'] = np.nan
-                    wallStateParam[mac]['period_stationary'] = np.nan
-                    wallStateParam[mac]['timeStamp_lastSignOfLife'] = np.nan
-                    wallStateParam[mac]['period_noSignOfLife'] = np.nan
                     wallStateParam[mac]['x_coord_multi'] = []
                     wallStateParam[mac]['y_coord_multi'] = []
                     wallStateParam[mac]['z_coord_multi'] = []
@@ -199,7 +192,7 @@ def decode_process_publish(mac, data):
                     wallStateParam[mac]['trackerInvalid'] = np.zeros((0))
                     wallStateParam[mac]['pandasDF'] = pd.DataFrame(columns=['timeStamp', 'trackIndex', 'numSubjects', 'roomOccupancy',
                                                                             'posX', 'posY', 'posZ', 'velX', 'velY', 'velZ', 'accX', 'accY', 'accZ', 
-                                                                            'bodyHeight', 'bodyWidth', 'state', 'kidOrAdult', 'signOfLife'])
+                                                                            'bodyHeight', 'bodyWidth', 'state', 'kidOrAdult'])
 
                 # Read parsed data from radar output dictionary
                 # Radar Trackers' Data Extraction
@@ -436,36 +429,6 @@ def decode_process_publish(mac, data):
                                 wallStateParam[mac]['trackPos'][minDistIdx] = [x_pos, y_pos, z_pos]
                                 wallStateParam[mac]['trackVelocity'][minDistIdx] = [x_vel, y_vel, z_vel]
 
-                                # Sign Of life
-                                if numTracks == 1:
-                                    if math.isnan(wallStateParam[mac]['x0']):
-                                        wallStateParam[mac]['x0'] = x_pos
-                                        wallStateParam[mac]['y0'] = y_pos
-                                        wallStateParam[mac]['z0'] = z_pos
-                                    else:
-                                        distanceMoved = np.abs(wallStateParam[mac]['x0'] - x_pos) + np.abs(wallStateParam[mac]['y0'] - y_pos) + np.abs(wallStateParam[mac]['z0'] - z_pos)
-                                        wallStateParam[mac]['x0'] = x_pos
-                                        wallStateParam[mac]['y0'] = y_pos
-                                        wallStateParam[mac]['z0'] = z_pos
-                                        if distanceMoved > 0.1 or math.isnan(wallStateParam[mac]['timeStamp_stationary']):
-                                            wallStateParam[mac]['timeStamp_stationary'] = ts
-                                        else:
-                                            wallStateParam[mac]['period_stationary'] = ts - wallStateParam[mac]['timeStamp_stationary']
-                                            if len(x_coord[trackIndices == trackId]) > 0 or math.isnan(wallStateParam[mac]['period_noSignOfLife']):
-                                                wallStateParam[mac]['timeStamp_lastSignOfLife'] = ts
-                                            else:
-                                                wallStateParam[mac]['period_noSignOfLife'] = ts - wallStateParam[mac]['timeStamp_lastSignOfLife']
-                                                if wallStateParam[mac]['period_noSignOfLife'] > 10 and wallStateParam[mac]['period_stationary'] > 30:
-                                                    wall_Dict['signOfLife'] = 0
-                                                    
-                                                    # Publish alert via MQTT communication channel
-                                                    pubPayload = {"TIMESTAMP":ts, "URGENCY":3, "TYPE":1, "DETAILS":"NOSIGNOFLIFE"}
-                                                    jsonData = json.dumps(pubPayload)
-                                                    mqttc.publish("/GMT/DEV/"+mac+"/ALERT", jsonData)
-
-                                                else:
-                                                    wall_Dict['signOfLife'] = 1
-
                                 # Multi-Frame Aggregation
                                 wallStateParam[mac]['x_coord_multi'][minDistIdx].append(x_coord[trackIndices == trackId])
                                 wallStateParam[mac]['y_coord_multi'][minDistIdx].append(y_coord[trackIndices == trackId])
@@ -641,7 +604,6 @@ def decode_process_publish(mac, data):
                                     else:
                                         aggregate_dict['state'] = np.nan
                                     aggregate_dict['kidOrAdult'] = pandasDF_dum['kidOrAdult'].mean(skipna=True)
-                                    aggregate_dict['signOfLife'] = pandasDF_dum['signOfLife'].mean(skipna=True)
 
                                     # if aggregate_dict['state'].dropna().empty:
                                     # print(aggregate_dict)
@@ -673,9 +635,6 @@ def decode_process_publish(mac, data):
                                         aggregate_dict['numSubjects'] = int(round(aggregate_dict['numSubjects']))
                                     if not math.isnan(aggregate_dict['roomOccupancy']):
                                         aggregate_dict['roomOccupancy'] = bool(round(aggregate_dict['roomOccupancy']))
-                                    if not math.isnan(aggregate_dict['signOfLife']):
-                                        aggregate_dict['signOfLife'] = bool(round(aggregate_dict['signOfLife']))
-
                                     for key, value in aggregate_dict.items():
                                         if str(value)[0:3] == 'nan':
                                             aggregate_dict[key] = None
@@ -754,7 +713,6 @@ def decode_process_publish(mac, data):
                             aggregate_dict['bodyWidth'] = None
                             aggregate_dict['state'] = None
                             aggregate_dict['kidOrAdult'] = None
-                            aggregate_dict['signOfLife'] = None
 
                             # print(aggregate_dict['state'])
                             dict_copy = copy.deepcopy(aggregate_dict)
